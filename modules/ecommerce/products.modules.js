d65070ef76b2c7daeedc216d1f8f71ff3edca216
shyam more <skumarmoure@gmail.com>
2024-08-06 20:47:01 +0530

    add ecommerce file

---
+++ modules/ecommerce/products.modules.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    productImg: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    ownername: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
