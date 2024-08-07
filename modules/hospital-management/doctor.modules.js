1f84d76818404e0f951468f385ec30ab564f0920
shyam more <skumarmoure@gmail.com>
2024-08-07 12:51:25 +0530

    add file hospital-mang

---
+++ modules/hospital-management/doctor.modules.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: String,
+      required: true,
+    },
+    qualification: {
+      type: String,
+      required: true,
+    },
+    experiencedInYear: {
+      type: Number,
+      required: true,
+    },
+    workInHospital: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital",
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Doctor = mongoose.model("Doctor", doctorSchema);
