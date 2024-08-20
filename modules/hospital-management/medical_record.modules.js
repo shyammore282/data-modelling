6cb75a1c9a5736584993e001649615275bd006ae
shyam more <skumarmoure@gmail.com>
2024-08-20 20:13:36 +0530

    add medical-record file

--- modules/hospital-management/medical_record.modules.js
+++ modules/hospital-management/medical_record.modules.js
@@ -1,6 +1,39 @@
 import mongoose from "mongoose";
 
-const medicalRecordSchema = new mongoose.Schema({}, { timestamps: true });
+const medicalRecordSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    dignosedWith: {
+      type: String,
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["M", "F", "O"],
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    doctorname: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const MedicalRecord = mongoose.model(
   "MedicalRecord",
